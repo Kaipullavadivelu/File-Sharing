8f07558e386539c75050b99f78419e6d7abdd817
Kaipullavadivelu <86786455+Kaipullavadivelu@users.noreply.github.com>
2022-07-12 19:30:29 +0530

    done

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>This Bot is only for @TamilanLinkz.\n\nCreated By: @BaashaXBoT</a>\",
+            text = f"<b>This Bot is only for @TamilanLinkz.\n\nCreated By: @BaashaXBoT</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
